c5842d563953439a7f7e2a40e255cc4d06220114
pcy112 <123913503+pcy112@users.noreply.github.com>
2023-01-30 10:47:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,13 @@ def image_to_byte(img):
     img.save(buffer, format='JPEG')
     return buffer
 
-# unsafe_allow_html: html 태그를 넣을 수 있음
-st.write("# Style transfer")
-st.write("### AI를 이용한 나만의 웹어플!")
-
+# 제목
+st.markdown('<p style="font-family:Courier; color:#FFA500; font-size: 40px;">양식 바꾸기</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-family:Courier; color:#FF8C00; font-size: 24px;">인공지능을 이용한 나만의 편리한 인터넷!</p>', unsafe_allow_html=True)
 # 사이드 바 만들기
 models = os.listdir(root_model)
 style_name = st.sidebar.selectbox(
-    'Select Style',
+    '양식',
     ('custom' if i == len(models) else models[i].split('.')[0] for i in range(len(models) + 1))
 )
 if style_name != 'custom':
@@ -36,7 +35,7 @@ else:
 # 스타일을 바꿀 사진 업로드
 img = None
 uploaded_file = st.file_uploader(
-    "이미지를 골라주세요", type=["jpg", "jpeg"], key = 'img')
+    "사진을 선택해주세요", type=["jpg", "jpeg"], key = 'img')
 
 show_file = st.empty()
 
@@ -47,37 +46,35 @@ if uploaded_file:
     img.save(input_path, "JPEG")
     
     # 업로드한 이미지 띄워주기
-    st.image(img, caption='내가 올린 이미지', use_column_width=True)
+    st.image(img, caption="내가 선택한 사진", use_column_width=True)
     
     # 이미 학습된 스타일을 선택하면 스타일 이미지 띄우기
     if path_style != None:
-        st.image(path_style, caption='스타일을 입힐 이미지', use_column_width=True)
+        st.image(path_style, caption='양식을 입힐 사진', use_column_width=True)
     # 커스텀을 선택하면 스타일 이미지를 업로드
     else:
         img2 = None
-        uploaded_st_file = st.file_uploader(
-            "입히고 싶은 이미지를 골라주세요.", type=["jpg", "jpeg"], key = 'style')
+        uploaded_st_file = st.file_uploader( 
+            "입히고 싶은 이미지 선택", type=["jpg", "jpeg"], key = 'style')
         
         # 스타일 이미지가 업로드 됐는지 확인
         if uploaded_st_file:
             img2 = Image.open(uploaded_st_file)
             img2.save(style_path, "JPEG")
-            st.image(img2, caption='내가 올린 스타일 이미지', use_column_width=True)
+            st.image(img2, caption='완성된 사진', use_column_width=True)
             
             # 학습할 때 사용할 이미지들의 크기 선택
             img_size = st.select_slider(
-                    '이미지 사이즈를 골라주세요.',
-                    options=[8, 16, 32, 64, 128, 256, 512])
-            st.write('이미지 사이즈가 크면 결과는 좋아질 수 있지만 학습이 오래 걸리거나 학습이 되지 않을 수 있습니다.')
+                    '사진의 크기',
+                    options=[8,16,32,64,128,256,512])
+            st.markdown('<p style="font-family:Courier; color:#8B0000; font-size: 15px;">사진의 크기가 크면 결과는 좋아질 수 있지만 학습이 오래걸리거나 학습이되지 않을 수 있습니다.</p>', unsafe_allow_html=True)
             # 학습 횟수 선택
             epoch = st.select_slider(
-                    '학습 횟수를 골라주세요',
-                    options=[1, 2, 3, 4, 5])
-            st.write('학습 횟수가 늘어나면 결과는 좋아질 수도 있지만 그만큼 학습이 오래 걸립니다.')
-            
-            
+                    '학습 횟수',
+                    options=[1,2,3,4,5])
+            st.markdown('<p style="font-family:Courier; color:#8B0000; font-size: 14px;">학습 횟수가 늘어나면 결과는 좋아질 수도 있지만 그만큼 학습이 오래 걸립니다.</p>', unsafe_allow_html=True)
+           
 extensions = [".png", ".jpeg", ".jpg"]
-
 if uploaded_file is not None and any(extension in uploaded_file.name for extension in extensions):
 
     name_file = uploaded_file.name.split(".")
@@ -89,10 +86,10 @@ if uploaded_file is not None and any(extension in uploaded_file.name for extensi
 
     root_output = "./images/sample"
 
-    stylize_button = st.button("Stylize")
+    stylize_button = st.button("양식 입히기")
     
     if path_style == None:
-        st.warning("Streamlit 배포 환경에서는 학습하는 것을 권장하지 않습니다(약 10시간 이상 필요). PC에서 local로 진행해 주시기 바랍니다.")
+        st.markdown('<p style="font-family:Courier; color:#B22222; font-size: 14px;">Streamlit 배포 환경에서는  학습하는 것을 권장하지 않습니다.(약 10시간 이상 필요).PC에서 local로 진행해 주시기 바랍니다.</p>', unsafe_allow_html=True)
           
     if stylize_button:
         try:
@@ -104,11 +101,11 @@ if uploaded_file is not None and any(extension in uploaded_file.name for extensi
             stylized = style.stylizing(model_path, root_output, input_path)
             
             # 결과 이미지 출력
-            st.write("### Output Image")
+            st.markdown('<p style="font-family:Courier; color:#32CD32; font-size: 20px;">완성된 사진</p>', unsafe_allow_html=True)
             st.image(stylized, width=400, use_column_width=True)
             # 다운로드 버튼을 눌러 만들어진 이미지 내려받기
             st.download_button(
-                label="Download Image",
+                label="사진을 내려받기",
                 data=image_to_byte(stylized),
                 file_name=f"{style_name}_{name_file[0]}.jpg"
             )
