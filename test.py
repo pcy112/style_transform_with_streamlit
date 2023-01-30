08e4b0e2f16eba4b356760b176a047c739dc9bb7
pcy112 <123913503+pcy112@users.noreply.github.com>
2023-01-30 11:26:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,49 +2,17 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 
-input_path = "./test/input.jpg"
-
-# 이미지를 바이트로 바꾸어 주는 함수
-# st.download_button은 바이트 데이터만 읽을 수 있음
 def image_to_byte(img):
     buffer = BytesIO()
     img.save(buffer, format='JPEG')
     return buffer
 
-# text 써보기
 st.write("# 내 페이지")
-st.write("### 테스트 페이지입니다.")
-
-# 사이드 바 만들기
-side_list = ['내용1', '내용2', '내용3', '내용4']
-style_name = st.sidebar.selectbox(
-    'Select Style',
-    side_list
-)
-
-# 사진 업로드
-img = None
-uploaded_file = st.file_uploader(
-    "이미지를 골라주세요", type=["jpg", "jpeg"], key = 'img')
-
-# 파일이 업로드 됐는지 확인
-if uploaded_file:
-    #업로드한 이미지 열기
-    img = Image.open(uploaded_file)
-    img.save(input_path, "JPEG")
-    
-    # 업로드한 이미지 띄워주기
-    st.image(img, caption='내가 올린 이미지', use_column_width=True)
-    
-    # 숫자를 고를 수 있는 슬라이더 만들기
-    num = st.select_slider(
-            '숫자를 골라주세요',
-            options=[1, 2, 3, 4, 5])
-    st.write(num)
-    
-    # 이미지를 내려 받을 수 있는 버튼 만들기
-    st.download_button(
-        label="이미지 다운로드",
-        data=image_to_byte(img),
-        file_name='output_img.jpg'
-    )
+st.write("## 테스트 페이지입니다.")
+d = st.sidebar.selectbox('Select Style',['1개의 사진', '2개의 사진','3개의 사진','4개의 사진'])
+for i in range(1, 5):
+    if d >= f'{i}개의 사진':
+        img = st.file_uploader(f"{i}번째 이미지를 골라주세요", type = ['jpg','jpeg'],key = f'이미지{i}')
+        if img:
+            img = Image.open(img)
+            st.image(img, caption = f"내가 {i}번째로 올린 이미지")
